8cd4b61d477373b0b5e7c5dd0c53dae32d166104
Setzra <jonkoenes@gmail.com>
2020-01-15 22:35:25 -0800

    Adding in solution files with all extra credit

--- src/write-data.js
+++ src/write-data.js
@@ -5,6 +5,7 @@ const dynamodb = new AWS.DynamoDB.DocumentClient({
   endpoint: new AWS.Endpoint('http://localhost:8000'),
   region: 'us-west-2',
   // what could you do to improve performance?
+  convertResponseTypes: false
 });
 
 const tableName = 'SchoolStudents';
@@ -24,4 +25,28 @@ exports.handler = (event) => {
   // TODO validate that all expected attributes are present (assume they are all required)
   // TODO use the AWS.DynamoDB.DocumentClient to save the 'SchoolStudent' record
   // The 'SchoolStudents' table key is composed of schoolId (partition key) and studentId (range key).
+  let validParams = ["schoolId", "schoolName", "studentId", "studentFirstName", "studentLastName", "studentGrade"]
+
+  var doc = {}
+
+  // Loop through our list of required params. Error on bad, add on good
+  for (i = 0; i < validParams.length; i++) {
+  	// If null or undefined, return error.
+  	if (event[validParams[i]] == null || event[validParams[i]] == undefined) return Error("Not all params provided. Missing " + validParams[i])
+  	// If it's valid, add it to the doc
+  	doc[validParams[i]] = event[validParams[i]]
+  }
+
+  let submission = {
+  	TableName: tableName,
+  	Item: doc
+  }
+
+  dynamodb.put(submission, function(err, data){
+  	if (err) {
+  		console.error("Error submitting doc: " + JSON.stringify(err));
+  		return Error(err);
+  	}
+  });
+
 };
